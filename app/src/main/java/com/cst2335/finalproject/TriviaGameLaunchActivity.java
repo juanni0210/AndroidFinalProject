21570325b47e293dd79e9fed164a4f844c41dee7
juanni0210 <janeni1020@gmail.com>
2021-03-27 00:56:54 -0400

    Added toolbar and navigation drawer to TriviaGameLaunch page. Juan Ni

--- app/src/main/java/com/cst2335/finalproject/TriviaGameLaunchActivity.java
+++ app/src/main/java/com/cst2335/finalproject/TriviaGameLaunchActivity.java
@@ -1,10 +1,20 @@
 package com.cst2335.finalproject;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -12,14 +22,17 @@ import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
 /**
  * Launch page of trivia game where user can select question number, type, difficulty type and other options.
  * @author Juan Ni
  */
-public class TriviaGameLaunchActivity extends AppCompatActivity {
+public class TriviaGameLaunchActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, BottomNavigationView.OnNavigationItemSelectedListener {
     private String questionNum, difficultyType, questionType, gameURL;
     private EditText questionNumberInput;
     private Spinner difficultySpinner, questionTypeSpinner;
@@ -30,6 +43,10 @@ public class TriviaGameLaunchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        //full screen
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_trivia_game_launch);
 
         questionNumberInput = findViewById(R.id.questionNumInput);
@@ -114,6 +131,27 @@ public class TriviaGameLaunchActivity extends AppCompatActivity {
         });
 
 
+        //This gets the toolbar from the layout:
+        Toolbar tBar = (Toolbar)findViewById(R.id.toolbar);
+        //This loads the toolbar, which calls onCreateOptionsMenu below:
+        setSupportActionBar(tBar);
+
+        //For NavigationDrawer:
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer, tBar, R.string.navigationOpen, R.string.navigationClose);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        //For bottomNavigationBar
+        BottomNavigationView bnv = findViewById(R.id.bnv);
+        bnv.setOnNavigationItemSelectedListener(this);
+
+
     }
 
     /**
@@ -132,6 +170,94 @@ public class TriviaGameLaunchActivity extends AppCompatActivity {
         snackbar.show();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String message = null;
+        //Look at your menu XML file. Put a case for every id in that file:
+        switch(item.getItemId())
+        {
+            //what to do when the menu item is selected:
+            case R.id.triviaItem:
+                startActivity(new Intent(TriviaGameLaunchActivity.this, TriviaGameLaunchActivity.class));
+                break;
+            case R.id.songsterItem:
+                Toast.makeText(this, "Go to songster page", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.carDBItem:
+                Toast.makeText(this, "Go to car database page", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.soccerItem:
+                Toast.makeText(this, "Go to soccer game page", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.helpItem:
+                AlertDialog.Builder triviaHelpDialog = new AlertDialog.Builder(this);
+                triviaHelpDialog.setTitle(getResources().getString(R.string.triviaHelpTile))
+                        //What is the message:
+                        .setMessage(getResources().getString(R.string.triviaInstructions1) + "\n"
+                                + getResources().getString(R.string.triviaInstructions2) + "\n"
+                                + getResources().getString(R.string.triviaInstructions3) + "\n"
+                                + getResources().getString(R.string.triviaInstructions4) + "\n"
+                                + getResources().getString(R.string.triviaInstructions5))
+                        //What the No button does:
+                        .setNegativeButton(getResources().getString(R.string.closeHelpDialog), (click, arg) -> {
+                        })
+                        //Show the dialog
+                        .create().show();
+                break;
+        }
+        return true;
+    }
+
+    // Needed for the OnNavigationItemSelected interface:
+    @Override
+    public boolean onNavigationItemSelected( MenuItem item) {
+
+        String message = null;
+
+        switch(item.getItemId())
+        {
+            case R.id.triviaItem:
+                startActivity(new Intent(TriviaGameLaunchActivity.this, TriviaGameLaunchActivity.class));
+                break;
+            case R.id.songsterItem:
+                Toast.makeText(this, "Go to songster page", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.carDBItem:
+                Toast.makeText(this, "Go to car database page", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.soccerItem:
+                Toast.makeText(this, "Go to soccer game page", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.helpItem:
+                AlertDialog.Builder triviaHelpDialog = new AlertDialog.Builder(this);
+                triviaHelpDialog.setTitle(getResources().getString(R.string.triviaHelpTile))
+                        //What is the message:
+                        .setMessage(getResources().getString(R.string.triviaInstructions1) + "\n"
+                                + getResources().getString(R.string.triviaInstructions2) + "\n"
+                                + getResources().getString(R.string.triviaInstructions3) + "\n"
+                                + getResources().getString(R.string.triviaInstructions4) + "\n"
+                                + getResources().getString(R.string.triviaInstructions5))
+                        //What the No button does:
+                        .setNegativeButton(getResources().getString(R.string.closeHelpDialog), (click, arg) -> {
+                        })
+                        //Show the dialog
+                        .create().show();
+                break;
+        }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return false;
+    }
 
 
 }
